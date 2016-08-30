e9aa3a54963797e79b8f7d6df4c615a83c5011de
Jacob Fowles <js.fowles@gmail.com>
2016-08-30 12:07:21 -0600

    concantentaion practice

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,11 @@
 
-var numOp1 = 5;
-var numOp2 = 10;
-console.log(numOp1 + numOp2)
+var avgHeight = 145;
+var numEruptions = 4;
+console.log("Average eruption height today: " + avgHeight)
+
+var eruption1 = 112;
+var eruption2 = 148;
+var eruption3 = 160;
+var numEruptions = 3;
+var avg = (eruption1 + eruption2 + eruption3) / numEruptions;
+console.log("The average height on Old Faithfuls eruptions today is: " + avg)
