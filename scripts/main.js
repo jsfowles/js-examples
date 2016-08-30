9c2de9e1891b267018f82a6c301d8cbcdd63b672
Jacob Fowles <js.fowles@gmail.com>
2016-08-30 12:31:26 -0600

    while loop practice

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,19 @@ var eruption3 = 160;
 var numEruptions = 3;
 var avg = (eruption1 + eruption2 + eruption3) / numEruptions;
 console.log("The average height on Old Faithfuls eruptions today is: " + avg)
+
+var num = 10;
+while(num > 0){
+  console.log(num);
+  num--;
+}
+
+var numSheep = 4;
+var monthNumber = 1;
+var monthsToPrint = 12;
+
+while (monthNumber <= monthsToPrint) {
+  numSheep *= 4;
+  console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
+  monthNumber++;
+}
