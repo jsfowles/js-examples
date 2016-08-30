8d8d08701b87fed456f392921b711e38b3ac30e5
Jacob Fowles <js.fowles@gmail.com>
2016-08-30 14:43:48 -0600

    for loop practice

--- scripts/main.js
+++ scripts/main.js
@@ -16,6 +16,8 @@ while(num > 0){
   num--;
 }
 
+// while loops
+while loops
 var numSheep = 4;
 var monthNumber = 1;
 var monthsToPrint = 12;
@@ -24,4 +26,32 @@ while (monthNumber <= monthsToPrint) {
   numSheep *= 4;
   console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
   monthNumber++;
+}
+
+// for loops
+var numSheep = 4;
+var monthsToPrint = 12;
+
+for (var monthNumber = 1; monthNumber <= monthsToPrint; monthNumber++) {
+  numSheep = numSheep * 4;
+  console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
+}
+
+for (var num = 10; num > 0; num--) {
+		console.log(num);
+}
+
+var currentGen = 1;
+var totalGen = 19;
+var totalMW = 0;
+
+while (currentGen <= 4) {
+  totalMW += 62;
+  console.log("Generator #" + currentGen + " is on, adding 62 MW, for a total of " + totalMW + " MW!");
+  currentGen++;
+}
+
+for (currentGen = 5; currentGen <= totalGen; currentGen++) {
+  totalMW += 124;
+  console.log("Generator #" + currentGen + " is on, adding 124 MW, for a total of " + totalMW + " MW!");
 }
